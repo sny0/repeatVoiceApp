998d9600279e50f57b6950c4770b118225cddec7
KRESOL_LAPTOP\shiny <shinya31415926535@gmail.com>
2023-05-23 15:21:13 +0900

    入力時間を設定

--- repeatVoice.py
+++ repeatVoice.py
@@ -2,17 +2,19 @@ import speech_recognition as sr
 # import pyttsx3
 import voicevox
 
+input_time = 20 # 入力時間[s]
 r = sr.Recognizer()
 # engine = pyttsx3.init()
 
 with sr.Microphone() as source:
     print("話しかけてください:")
-    audio = r.listen(source)
+    audio = r.listen(source, input_time)
+    print("認識中です・・・")
 
 try:
     text = r.recognize_google(audio, language="ja-JP")
     print("認識結果:", text)
-    voicevox.text_to_speech(text, 3)
+    voicevox.text_to_speech(text, 1)
     # engine.say(text)
     # engine.runAndWait()
 except sr.UnknownValueError:
